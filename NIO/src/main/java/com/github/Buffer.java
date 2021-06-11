d3435cd9f68c5b79b277e76e618da51e440064a0
andochiwa <a1066079469@gmail.com>
2021-06-12 04:53:06 +0900

    buffer read

--- NIO/src/main/java/com/github/Buffer.java
+++ NIO/src/main/java/com/github/Buffer.java
@@ -1,6 +1,9 @@
 package com.github;
 
-import java.nio.IntBuffer;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  * @author HAN
@@ -10,15 +13,23 @@ import java.nio.IntBuffer;
 public class Buffer {
     public static void main(String[] args){
         // Buffer的使用
-        IntBuffer intBuffer = IntBuffer.allocate(5);
-        for (int i = 0; i < intBuffer.capacity(); i++) {
-            intBuffer.put(i);
+        try (FileChannel channel = new FileInputStream("D:\\code\\java\\Netty\\NIO\\src\\main\\resources\\file01.txt")
+                .getChannel()) {
+            // 读取文件数据
+            ByteBuffer byteBuffer = ByteBuffer.allocate(5);
+            // 重复去读取数据
+            for (int read = channel.read(byteBuffer); read != -1; read = channel.read(byteBuffer)) {
+                // 切换到读模式
+                byteBuffer.flip();
+                // 读取buffer内数据
+                while (byteBuffer.hasRemaining()) {
+                    System.out.print((char) byteBuffer.get());
+                }
+                // 切换到写模式
+                byteBuffer.clear();
+            }
+        } catch (IOException ignored) {
         }
-        // 将buffer转换，读写切换
-        intBuffer.flip();
-        // 取数据
-        while (intBuffer.hasRemaining()) {
-            System.out.println(intBuffer.get());
-        }
+
     }
 }
